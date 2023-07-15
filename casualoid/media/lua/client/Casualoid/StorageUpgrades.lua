b46b91ae9a04bfa8d44a4cd7d898bdcca690d862
mxswat <massimocacciapaglia.13@gmail.com>
2023-07-15 21:02:51 +0100

    feat: implements storag upgrades

---
+++ casualoid/media/lua/client/Casualoid/StorageUpgrades.lua
@@ -0,0 +1,25 @@
+local getUpgradeItem = function(inputItems)
+  for i = 0, (inputItems:size() - 1) do
+    local item = inputItems:get(i);
+    if item:getType() == 'StorageUpgrade' then
+      return item
+    end
+  end
+
+  return nil
+end
+
+function ImproveStorageUpgrade(inputItems, resultItem, player)
+  local inputItem = getUpgradeItem(inputItems)
+
+  if not inputItem then
+    return
+  end
+
+  local newWeight = math.ceil(inputItem:getActualWeight() * 2.1)
+
+  resultItem:setActualWeight(newWeight + 0.01);
+  resultItem:setWeight(newWeight + 0.01);
+  resultItem:setName("Storage Upgrade " .. newWeight * -1 .. "kg");
+  resultItem:setCustomName(true);
+end
