38779190a1bd01086d3e96e845b8e06cd79e4ddf
mxswat <massimocacciapaglia.13@gmail.com>
2023-11-08 22:35:36 +0000

    new Disassemble hooked version works!

--- Contents/mods/casualoid/media/lua/client/Casualoid/Disassemble.lua
+++ Contents/mods/casualoid/media/lua/client/Casualoid/Disassemble.lua
@@ -1,52 +1,49 @@
--- "Disassemble Container With Items" mod, Mx's version
+local Hooks = require("MxUtilities/Hooks")
 
-local function removeLineFromInfoTable(_table, _lineA)
-  for i, line in ipairs(_table) do
+local Disassemble = {}
+
+function Disassemble:getInfoPanelDescription(_square, _object, _player, _mode)
+  local infoTable = Hooks:getLastResult()
+  if not (InfoPanelFlags.hasItems and _mode == "scrap") then
+    return infoTable
+  end
+
+  for i, line in ipairs(infoTable) do
     for _, entry in ipairs(line) do
-      if entry.txt == _lineA then
-        table.remove(_table, i)
-        return true
+      if entry.txt == "- " .. getText("IGUI_ItemsInContainer") then
+        table.remove(infoTable, i)
+        return infoTable
       end
     end
-  end
-  return false
-end
-
-local old_ISMoveableSpriteProps_getInfoPanelDescription = ISMoveableSpriteProps.getInfoPanelDescription
-function ISMoveableSpriteProps:getInfoPanelDescription(_square, _object, _player, _mode)
-  local infoTable = old_ISMoveableSpriteProps_getInfoPanelDescription(self, _square, _object, _player, _mode)
-
-  if InfoPanelFlags.hasItems and _mode == "scrap" then
-    removeLineFromInfoTable(infoTable, "- " .. getText("IGUI_ItemsInContainer"))
   end
 
   return infoTable
 end
 
-local old_ISMoveableSpriteProps_scrapObjectInternal = ISMoveableSpriteProps.scrapObjectInternal
-function ISMoveableSpriteProps:scrapObjectInternal(_character, _scrapDef, _square, _object, _scrapResult, _chance,
-                                                   _perkName)
-  local result = old_ISMoveableSpriteProps_scrapObjectInternal(self, _character, _scrapDef, _square, _object,
-    _scrapResult, _chance, _perkName)
+function Disassemble:scrapObjectInternal(_character, _scrapDef, _square, _object, ...)
+  local result = Hooks:getLastResult()
 
-  local object = _object;
-  if _scrapDef and object and _square then
-    if not (object:isFloor() and (_square:getZ() == 0)) then
-      for i = 1, object:getContainerCount() do
-        local container = object:getContainerByIndex(i - 1)
-        for j = 1, container:getItems():size() do
-          object:getSquare():AddWorldInventoryItem(container:getItems():get(j - 1), 0.0, 0.0, 0.0)
-        end
-      end
+  local object = _object
+  if not (_scrapDef and object and _square) then
+    return result
+  end
+
+  if object:isFloor() and (_square:getZ() == 0) then
+    return result
+  end
+
+  for i = 1, object:getContainerCount() do
+    local container = object:getContainerByIndex(i - 1)
+    for j = 1, container:getItems():size() do
+      object:getSquare():AddWorldInventoryItem(container:getItems():get(j - 1), 0.0, 0.0, 0.0)
     end
   end
 
   return result
 end
 
-local old_ISMoveableSpriteProps_canScrapObjectInternal = ISMoveableSpriteProps.canScrapObjectInternal
-function ISMoveableSpriteProps:canScrapObjectInternal(_result, _object)
-  local canScrap = old_ISMoveableSpriteProps_canScrapObjectInternal(self, _result, _object)
+function Disassemble:canScrapObjectInternal(_result, _object)
+  local canScrap = Hooks:getLastResult()
 
   if canScrap == false and self:objectNoContainerOrEmpty(_object) == false then
     return true
@@ -54,3 +51,11 @@ function ISMoveableSpriteProps:canScrapObjectInternal(_result, _object)
 
   return canScrap
 end
+
+Events.OnGameStart.Add(function()
+  if not SandboxVars.Casualoid.DisassembleContainerWithItems then
+    return
+  end
+
+  Hooks:autoApplyPostHooks(ISMoveableSpriteProps, Disassemble)
+end);
