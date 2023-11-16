d51c3cb18bb9029f4b2345ee0b07c4f0593f9d2b
mxswat <massimocacciapaglia.13@gmail.com>
2023-11-16 01:29:46 +0000

    scroll seems ok

--- Contents/mods/casualoid/media/lua/client/Casualoid/NamedContainers/NamedContainersUI.lua
+++ Contents/mods/casualoid/media/lua/client/Casualoid/NamedContainers/NamedContainersUI.lua
@@ -1,61 +1,33 @@
 require "ISUI/ISInventoryPage"
-
+local SetWidthDialog = require "Casualoid/NamedContainers/SetWidthDialog"
+local NamedContainersUIData = require "Casualoid/NamedContainers/NamedContainersUIData"
 local Hooks = require "MxUtilities/Hooks"
-local Debug = require("Casualoid/Debug")
+local Debug = require "Casualoid/Debug"
 
 ---@class NamedContainersUI: ISInventoryPage
 local NamedContainersUI = {}
 
-function NamedContainersUI.saveSize(self)
-  -- save the data
-  local namedContainersUIdata = NamedContainersUI.getModData()
-  if self.onCharacter then
-    namedContainersUIdata.sizeCharacter = self.iconsHeader:getWidth()
-    return
-  end
-  namedContainersUIdata.sizeLoot = self.iconsHeader:getWidth()
-end
-
-
-function NamedContainersUI.getSavedSize(self)
-  local modData = NamedContainersUI.getModData()
-  if self.onCharacter then
-    return modData.sizeCharacter
-  end
-
-  return modData.sizeLoot
-end
-
-function NamedContainersUI.getModData()
-  ---@class NamedContainersUIModata
-  ---@field sizeLoot number
-  ---@field sizeCharacter number
-  local modData = ModData.getOrCreate('NamedContainersUI')
-
-  modData.sizeLoot = modData.sizeLoot or 100
-  modData.sizeCharacter = modData.sizeCharacter or 100
-
-  return modData
-end
-
 function NamedContainersUI:createChildren()
-  Debug:print('NamedContainersUI:createChildren', 'self.onCharacter', self.onCharacter)
-end
-
-function NamedContainersUI:addContainerButton()
-  ---@type ISButton
-  local button = Hooks:GetReturn()
-  button:setY(button:getY() + self.iconsHeader:getHeight() + 1)
-  button:setWidth(self.iconsHeader:getWidth())
-  button:setX(self.iconsHeader.x)
+  self.inventoryPane.anchorRight = false;
 end
 
 function NamedContainersUI.onResizeIconsColumn(self, button)
-  self.inventoryPane:setWidth(self.iconsHeader.x - 1)
+  self.inventoryPane:setWidth(self.iconsHeader.x)
+
+  if self.iconsHeader.x <= (self.inventoryPane.typeHeader.x + 100) then
+    self.inventoryPane.typeHeader:resize(100)
+    self.inventoryPane:onResizeColumn(self.inventoryPane.typeHeader)
+  end
+
+  self.inventoryPane:recalcSize()
+  self.inventoryPane.vscroll:setX(self.inventoryPane.width - self.inventoryPane.vscroll.width)
+  self:updateScrollbars();
+  -- self.inventoryPane:refreshContainer()
 
   ---@param button ISButton
   for _, button in ipairs(self.backpacks) do
-    button:setWidth(self.iconsHeader:getWidth())
+    button:setY(button:getY() + self.iconsHeader:getHeight() + 1)
+    button:setWidth(NamedContainersUIData.getSavedSize(self))
     button:setX(self.iconsHeader.x)
     button:setBorderRGBA(0.6, 0.6, 0.6, 0.5)
     if button.inventory ~= self.inventory then
@@ -65,6 +37,7 @@ function NamedContainersUI.onResizeIconsColumn(self, button)
 
   if button then
     NamedContainersUI.saveSize(self)
+    -- self:refreshBackpacks()
   end
 end
 
@@ -78,51 +51,40 @@ function ISInventoryPage.drawRectBorder(self, x, y, w, h, ...)
       and w == self.buttonSize
       and h == height - titleBarHeight - 7
   then
-    x = self:getWidth() - self.iconsHeader:getWidth()
-    w = self.iconsHeader:getWidth()
+    x = self:getWidth() - NamedContainersUIData.getSavedSize(self)
+    w = NamedContainersUIData.getSavedSize(self)
   end
 
   return old_drawRectBorder(self, x, y, w, h, ...)
 end
 
-local old_ISInventoryPage_refreshBackpacks = ISInventoryPage.refreshBackpacks
-function ISInventoryPage:refreshBackpacks()
+---@param reason string
+function NamedContainersUI.onRefreshBackpacks(self, reason)
+  if reason ~= "buttonsAdded" then return end
   if self.iconsHeader then
     self:removeChild(self.iconsHeader)
   end
 
-  local size = NamedContainersUI.getSavedSize(self)
+  local size = NamedContainersUIData.getSavedSize(self)
+  self.minimumWidth = 256 + size
   local x = self.width - size
   local y = self:titleBarHeight()
   local fontHgtSmall = getTextManager():getFontHeight(UIFont.Small)
-  self.iconsHeader = ISResizableButton:new(x, y, size, fontHgtSmall + 1, "~");
+  self.iconsHeader = ISButton:new(x, y, size, fontHgtSmall + 1, "~", self, SetWidthDialog.open);
   self.iconsHeader.borderColor.a = 0.2;
   self.iconsHeader:setBackgroundRGBA(0.0, 0.0, 0.0, 0.0)
   self.iconsHeader:setBackgroundColorMouseOverRGBA(0.3, 0.3, 0.3, 1.0)
   self.iconsHeader:setTextureRGBA(1.0, 1.0, 1.0, 1.0)
-  self.iconsHeader.minimumWidth = self.buttonSize
-  self.iconsHeader.resizeLeft = true
+  self.iconsHeader.anchorLeft = false
+  self.iconsHeader.anchorTop = false
   self.iconsHeader.anchorRight = true
-  self.iconsHeader.maximumWidth = 200
-  self.iconsHeader:initialise();
-  self.iconsHeader.onresize = { NamedContainersUI.onResizeIconsColumn, self, self.iconsHeader }
+  self.iconsHeader.anchorBottom = false
+  self.iconsHeader:initialise()
   self:addChild(self.iconsHeader);
 
-  old_ISInventoryPage_refreshBackpacks(self)
   NamedContainersUI.onResizeIconsColumn(self)
 end
 
-function NamedContainersUI:render()
-  if not self.iconsHeader then
-    return
-  end
-  local resize = self.iconsHeader.mouseOverResize or self.iconsHeader.resizing
-
-  if resize then
-    local height = self.inventoryPane:getHeight()
-    self:repaintStencilRect(self.inventoryPane:getRight() + 1, self.inventoryPane.y, 2, height)
-    self:drawRectStatic(self.inventoryPane:getRight() + 1, self.inventoryPane.y, 2, height, 0.5, 1, 1, 1)
-  end
-end
+Events.OnRefreshInventoryWindowContainers.Add(NamedContainersUI.onRefreshBackpacks)
 
 Hooks:PostHooksFromTable(ISInventoryPage, NamedContainersUI, 'NamedContainersUI')
---
+++ Contents/mods/casualoid/media/lua/client/Casualoid/NamedContainers/SetWidthDialog.lua
@@ -0,0 +1,32 @@
+local NamedContainersUIData = require "Casualoid/NamedContainers/NamedContainersUIData"
+local SetWidthDialog = {}
+
+---@param inventoryPage ISInventoryPage
+function SetWidthDialog.open(inventoryPage)
+  local minWidth = inventoryPage.buttonSize
+  local modalTitle = getText("IGUI_SetButtonsWidth") .. " | Min:"..(tostring(minWidth)).." Max:400"
+  local size = NamedContainersUIData.getSavedSize(inventoryPage)
+
+  -- self.speedScale = ISSliderPanel:new(self.width / 2 - 400 / 2, self.bottomPanel.y - 30, 400, 20, self, self.onSpeedScaleChanged)
+	-- self.speedScale.anchorTop = false
+	-- self.speedScale.anchorBottom = true
+	-- self.speedScale:setValues(0.0, 5.0, 0.1, 1.0)
+	-- self.speedScale:setCurrentValue(1.0, true)
+	-- self:addChild(self.speedScale)
+
+  local modal = ISTextBox:new(0, 0, 280, 180, modalTitle, tostring(size), nil, nil, nil, nil)
+  modal:initialise()
+  modal:setOnlyNumbers(true)
+  modal:addToUIManager()
+  modal.entry.onTextChange = function(entry)
+    local value = tonumber(entry:getInternalText()) or 0
+
+    local newSize = math.max(math.min(value, 400), minWidth)
+    NamedContainersUIData.saveSize(inventoryPage, newSize)
+
+    inventoryPage:refreshBackpacks()
+  end
+  modal.entry:focus()
+end
+
+return SetWidthDialog
