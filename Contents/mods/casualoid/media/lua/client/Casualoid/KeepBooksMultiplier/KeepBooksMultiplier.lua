4033a342eb59025a85bb6b5a7b48ad80d2a5f2fb
mxswat <massimocacciapaglia.13@gmail.com>
2023-11-30 00:26:54 +0000

    improved and fixed KeepBooksMultiplier on death :D

--- Contents/mods/casualoid/media/lua/client/Casualoid/KeepBooksMultiplier/KeepBooksMultiplier.lua
+++ Contents/mods/casualoid/media/lua/client/Casualoid/KeepBooksMultiplier/KeepBooksMultiplier.lua
@@ -1,43 +1,85 @@
 local Debug = require("Casualoid/Debug")
 
+---@class KeepBooksMultiplier
 local KeepBooksMultiplier = {}
 
-KeepBooksMultiplier.skillBooksCache = nil
-
+KeepBooksMultiplier.perkToSkillBookMap = nil
 
 function KeepBooksMultiplier:getModData()
   return ModData.getOrCreate("KeepBooksMultiplier");
 end
 
-function KeepBooksMultiplier:getSkillBooks()
-  if self.skillBooksCache then
-    return self.skillBooksCache
+---@param perk Perk
+---@param perkLevel number
+---@return Literature
+function KeepBooksMultiplier:getBookForPerk(perk, perkLevel)
+  --- skills start at 0, but if it as 0 it won't count the first book
+  local level = perkLevel + 1
+  local skillToBookMap = self:getSkillToBookMap()
+  -- Perk and getSkillTrained are not aligned inside `java\characters\skills\PerkFactory.java` so I need to use getName()
+  local perkName = perk:getName()
+  return skillToBookMap[perkName] and skillToBookMap[perkName][level]
+end
+
+function KeepBooksMultiplier:getSkillToBookMap()
+  if self.perkToSkillBookMap then
+    return self.perkToSkillBookMap
   end
 
-  self.skillBooksCache = {}
-  local allItems = getScriptManager():getAllItems()
-  for i = 1, allItems:size() do
-    local item = allItems:get(i - 1)
-    if item:getType() == Type.Literature then
-      if SkillBook[item:getSkillTrained()] then
-        table.insert(self.skillBooksCache, item)
-      end
+  self.perkToSkillBookMap = {}
+
+  local allItems = getAllItems()
+  local allItemsSize = allItems:size()
+  for i = allItemsSize - 1, 0, -1 do
+    local item = allItems:get(i)
+    if item:getType() == Type.Literature and SkillBook[item:getSkillTrained()] then
+      local instanceItem = item:InstanceItem(nil)
+      local skillTrained = item:getSkillTrained()
+      self.perkToSkillBookMap[skillTrained] = self.perkToSkillBookMap[skillTrained] or {}
+      self.perkToSkillBookMap[skillTrained][instanceItem:getLvlSkillTrained()] = instanceItem
+      self.perkToSkillBookMap[skillTrained][instanceItem:getMaxLevelTrained()] = instanceItem
     end
   end
 
-  return self.skillBooksCache
+  return self.perkToSkillBookMap
 end
 
 function KeepBooksMultiplier:save(player)
   local modData = self:getModData()
 
-  local skillBooks = self:getSkillBooks()
-  for _, book in ipairs(skillBooks) do
-    local readPages = player:getAlreadyReadPages(book:getFullName())
-    if readPages > 0 then
-      modData[book:getFullName()] = readPages
+  local perks = PerkFactory.PerkList;
+  for i = 0, perks:size() - 1 do
+    local perk = perks:get(i);
+    local level = player:getPerkLevel(perk)
+    local book = self:getBookForPerk(perk, level)
+    if book then
+      local readPages = player:getAlreadyReadPages(book:getFullType())
+      if readPages > 0 then
+        modData[book:getFullType()] = readPages
+      end
     end
   end
+end
+
+---@param player IsoGameCharacter
+---@param perk Perk
+function KeepBooksMultiplier:giveXpMultiplier(player, perk)
+  local level = player:getPerkLevel(perk)
+  local book = self:getBookForPerk(perk, level)
+  if not book then
+    return
+  end
+
+  local alreadyReadPages = player:getAlreadyReadPages(book:getFullType())
+  if alreadyReadPages <= 0 then
+    return
+  end
+
+  local mockAction = ISReadABook:new(player, book, 2)
+  Debug:print('PerkName:', perk, '|', 'Level:', level)
+  Debug:print('Apply XP multiplier from:', book:getDisplayName())
+  -- checkMultiplier sets the multiplier on the player too :D
+  ISReadABook.checkMultiplier(mockAction)
 end
 
 function KeepBooksMultiplier:load(player)
@@ -47,34 +89,28 @@ function KeepBooksMultiplier:load(player)
     player:setAlreadyReadPages(bookFullName, readPages)
   end
 
-  local skillBooks = self:getSkillBooks()
-  for _, skillBook in ipairs(skillBooks) do
-    local item = skillBook:InstanceItem(nil)
-    local mockAction = ISReadABook:new(player, item, 2)
-
-    local skillTrainedLevel = player:getPerkLevel(SkillBook[item:getSkillTrained()].perk) + 1
-    local isTooHighLvl = item:getLvlSkillTrained() > skillTrainedLevel
-    local isTooLowLvl = item:getMaxLevelTrained() < skillTrainedLevel
-    if item:getAlreadyReadPages() > 0 and not isTooHighLvl and not isTooLowLvl then
-      Debug:print('Apply XP multiplier from:', item:getDisplayName())
-      -- checkMultiplier sets the multiplier on the player too :D
-      ISReadABook.checkMultiplier(mockAction)
-    end
+  local skillToBookMap = self:getSkillToBookMap()
+  Debug:print('skillToBookMap')
+  Debug:printTable(skillToBookMap)
+  local perks = PerkFactory.PerkList;
+  for i = 0, perks:size() - 1 do
+    local perk = perks:get(i);
+    self:giveXpMultiplier(player, perk)
   end
 end
 
 function KeepBooksMultiplier:init()
-  Events.LevelPerk.Add(function(character)
+  Events.LevelPerk.Add(function(character, perk)
     -- To avoid tediousness, reapply existing multiplier on new level up
-    KeepBooksMultiplier:load(character)
+    self:giveXpMultiplier(character, perk)
   end);
 
   Events.OnCreatePlayer.Add(function(_, player)
-    KeepBooksMultiplier:load(player)
+    self:load(player)
   end);
 
   Events.OnPlayerDeath.Add(function(player)
-    KeepBooksMultiplier:save(player)
+    self:save(player)
   end)
 end
 
