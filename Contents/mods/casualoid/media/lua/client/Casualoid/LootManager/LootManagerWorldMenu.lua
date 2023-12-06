28c1cb2f03e51cc8ad0a804421e66b1d82057bb0
mxswat <massimocacciapaglia.13@gmail.com>
2023-12-06 00:41:03 +0000

    swapping iso object works

--- Contents/mods/casualoid/media/lua/client/Casualoid/LootManager/LootManagerWorldMenu.lua
+++ Contents/mods/casualoid/media/lua/client/Casualoid/LootManager/LootManagerWorldMenu.lua
@@ -7,7 +7,7 @@ local LootManagerWorldMenu = {}
 ---@param customName string
 ---@param groupName string
 ---@return IsoObject?
-function LootManagerWorldMenu:findByNameAndGroup(worldObjects, groupName, customName)
+function LootManagerWorldMenu:findObjectByNameAndGroup(worldObjects, groupName, customName)
   for _, object in ipairs(worldObjects) do
     local props = object:getSprite():getProperties()
     local objCustomName = props and props:Val('CustomName')
@@ -21,23 +21,53 @@ end
 
 -- ISGeneratorInfoWindow
 
+---@param spriteName string # the sprite name eg: `loot_manager_0`
+---@return string # returns the sprite name with the last digit plus 4, wrapped between 0 and 7
+function LootManagerWorldMenu:getOppositeStateSpriteName(spriteName)
+  local lastDigit = tonumber(spriteName:sub(-1))
+
+  return spriteName:sub(1, -2) .. (lastDigit + 4) % 8
+end
+
+---@param object IsoObject
+---@param character IsoPlayer 
+function LootManagerWorldMenu:toggleLootManager(object, character)
+  local spriteName = self:getOppositeStateSpriteName(object:getSprite():getName())
+  -- object:setSprite(spriteName) -- this only changes the square tile estetics, not the actual object, good to know
+  local square = object:getSquare() 
+  square:transmitRemoveItemFromSquare(object)
+
+  local tileCursor = ISBrushToolTileCursor:new(spriteName, spriteName, character)
+
+  tileCursor:tryBuild(square:getX(),square:getY(), square:getZ())
+end
+
 ---@param playerIndex int
 ---@param context ISContextMenu
 ---@param worldObjects table
-function LootManagerWorldMenu.renderPlaceMenu(playerIndex, context, worldObjects)
+function LootManagerWorldMenu.renderEnableMenu(playerIndex, context, worldObjects)
   local self = LootManagerWorldMenu
   local player = getSpecificPlayer(playerIndex)
 
-  local lootManagerObject = self:findByNameAndGroup(worldObjects, 'LootManager', 'Off')
+  local lootManagerObject = self:findObjectByNameAndGroup(worldObjects, 'LootManager', 'Off')
   if not lootManagerObject then return end
 
 
   local text = getText("ContextMenu_EnableLootManager")
-  local menuOption = context:addOption(text, worldObjects, ISCampingMenu.onPlaceCampfire, player, lootManagerObject);
+  local menuOption = context:addOption(text, self, LootManagerWorldMenu.toggleLootManager, lootManagerObject, player);
+
+  if not ISMoveableSpriteProps:objectNoContainerOrEmpty(lootManagerObject) then
+    menuOption.notAvailable = true
+    local tooltip = ISInventoryPaneContextMenu.addToolTip()
+    tooltip.description = getText("ContextMenu_EmptyTheLootManager")
+
+    return
+  end
+  -- On click use setSpriteFromName
 end
 
 function LootManagerWorldMenu:init()
-  Events.OnFillWorldObjectContextMenu.Add(LootManagerWorldMenu.renderPlaceMenu)
+  Events.OnFillWorldObjectContextMenu.Add(LootManagerWorldMenu.renderEnableMenu)
 end
 
 LootManagerWorldMenu:init()
