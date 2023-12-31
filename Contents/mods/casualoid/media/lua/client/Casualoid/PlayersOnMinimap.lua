666b4f288556db9bf2f99bbd6224c25d7be7c6bf
mxswat <massimocacciapaglia.13@gmail.com>
2023-12-31 00:19:37 +0000

    show players on minimap sandbox settings added!

--- Contents/mods/casualoid/media/lua/client/Casualoid/PlayersOnMinimap.lua
+++ Contents/mods/casualoid/media/lua/client/Casualoid/PlayersOnMinimap.lua
@@ -2,6 +2,10 @@ local old_ISMiniMap_InitPlayer = ISMiniMap.InitPlayer
 function ISMiniMap.InitPlayer(playerNum)
   local MINIMAP = old_ISMiniMap_InitPlayer(playerNum)
 
+  if not SandboxVars.Casualoid.ShowPlayersOnMinimap then
+    return MINIMAP
+  end
+
   local INNER = MINIMAP.inner
   INNER.mapAPI:setBoolean("RemotePlayers", true)
   INNER.mapAPI:setBoolean("PlayerNames", true)
